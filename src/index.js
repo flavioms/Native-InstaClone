b8faf4104e1d3d7fc69833fc7931f956cb175ab2
Flavio Martins <flaviomartins_silva@hotmail.com>
2019-10-06 19:02:12 -0300

    feature/List publications in feed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import {View, Text} from 'react-native';
-
-// import { Container } from './styles';
+import {StatusBar} from 'react-native';
+import Routes from './routes';
 
 export default function App() {
   return (
-    <View>
-      <Text>Hello World</Text>
-    </View>
+    <>
+      <StatusBar barStyle="dark-content" backgroundColor="#f5f5f5" />
+      <Routes />
+    </>
   );
 }
